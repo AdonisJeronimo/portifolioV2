de81e6beeace3a3f1954df75c81ee6311404ab04
AdonisJeronimo <adonisfjeronimo@gmail.com>
2023-05-04 11:35:09 -0300

    Aba habilidades e projetos-  mobile

--- src/js/script.js
+++ src/js/script.js
@@ -28,3 +28,18 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 };
 navLink.forEach(n => n.addEventListener('click',linkAction));
+
+// PORTFOLIO
+
+let swiper = new Swiper(".portfolio-container", {
+    cssMode: true,
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+  });
