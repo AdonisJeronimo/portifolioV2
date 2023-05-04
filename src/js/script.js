880c54ab03794c9919ee7c2a4e6bc0dcd9ee79e6
AdonisJeronimo <adonisfjeronimo@gmail.com>
2023-05-04 18:43:29 -0300

    Contato e Footer

--- src/js/script.js
+++ src/js/script.js
@@ -43,3 +43,31 @@ let swiper = new Swiper(".portfolio-container", {
       clickable:true,
     },
   });
+
+  //Change Backgound header
+
+  function scrollHeader(){
+    const nav = document.getElementById('header');
+
+    if(this.scrollY >= 80){
+        nav.classList.add('scroll-header')
+    }else{
+        nav.classList
+    }
+  };
+
+  window.addEventListener('scroll',scrollHeader)
+
+  //SHOW SCROLL-UP
+
+  function scrollUp(){
+    const scrollUp = document.getElementById('scroll_up');
+
+    if(this.scrollY >= 560){
+        scrollUp.classList.add('show-scroll')
+    }else{
+        scrollUp.classList.remove('show-scroll')
+    }
+  };
+
+  window.addEventListener('scroll',scrollUp);
